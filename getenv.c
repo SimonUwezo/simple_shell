6f71ff1a89c758734a738f2159340619a8673ddf
Sododa <144449305+Sododa@users.noreply.github.com>
2023-12-13 23:09:22 +0300

    Update getenv.c

--- getenv.c
+++ getenv.c
@@ -25,7 +25,7 @@ char **get_environ(info_t *info)
 int _unsetenv(info_t *info, char *var)
 {
 	list_t *node = info->env;
-	size_t i = 0;
+	size_t b = 0;
 	char *p;
 
 	if (!node || !var)
@@ -36,13 +36,13 @@ int _unsetenv(info_t *info, char *var)
 		p = starts_with(node->str, var);
 		if (p && *p == '=')
 		{
-			info->env_changed = delete_node_at_index(&(info->env), i);
-			i = 0;
+			info->env_changed = delete_node_at_index(&(info->env), b);
+			b = 0;
 			node = info->env;
 			continue;
 		}
 		node = node->next;
-		i++;
+		b++;
 	}
 	return (info->env_changed);
 }
